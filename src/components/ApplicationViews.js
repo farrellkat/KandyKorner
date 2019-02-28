3891711a514599ce39a1df0b80b27d069c8941f2
James Farrell <jamesfarrellmusic@gmail.com>
2019-02-28 13:17:33 -0600

    delete button works

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -13,20 +13,32 @@ export default class ApplicationViews extends Component {
         candies: []
     }
 
+    discontinue = id => {
+        fetch(`http://localhost:5002/candies/${id}`, {
+            method: "DELETE"
+        }).then(() =>
+            fetch("http://localhost:5002/candies"))
+            .then(res => res.json())
+            .then(candies => this.setState({
+                candies: candies
+            })
+            )
+    }
+
     componentDidMount() {
         const newState = {}
 
-        fetch("http://localhost:5002/storeArray")
+        fetch("http://localhost:5002/stores")
             .then(r => r.json())
             .then(stores => newState.stores = stores)
-            .then(() => fetch("http://localhost:5002/employeeArray")
-            .then(r => r.json()))
+            .then(() => fetch("http://localhost:5002/employees")
+                .then(r => r.json()))
             .then(employees => newState.employees = employees)
-            .then(() => fetch("http://localhost:5002/candyTypeArray")
-            .then(r => r.json()))
+            .then(() => fetch("http://localhost:5002/candyTypes")
+                .then(r => r.json()))
             .then(candyTypes => newState.candyTypes = candyTypes)
-            .then(() => fetch("http://localhost:5002/candyArray")
-            .then(r => r.json()))
+            .then(() => fetch("http://localhost:5002/candies")
+                .then(r => r.json()))
             .then(candies => newState.candies = candies)
             .then(() => this.setState(newState))
     }
@@ -41,10 +53,10 @@ export default class ApplicationViews extends Component {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
                 <Route exact path="/candies" render={(props) => {
-                    return <CandyList candyTypes={this.state.candyTypes}
+                    return <CandyList discontinue={this.discontinue} candyTypes={this.state.candyTypes}
                         candies={this.state.candies} />
                 }} />
             </React.Fragment>
-            )
-        }
+        )
+    }
 }
--- src/components/CandyList.js
+++ src/components/CandyList.js
@@ -5,19 +5,30 @@ import React, { Component } from 'react'
 
 class CandyList extends Component {
     render() {
+
         return (
             <section className="candyList">
-                <h3 className="candyList">Candy List</h3>
+                <h3 className="candyListHeader">Candy List</h3>
+                <div className="candyContainer">
                 {
                     this.props.candyTypes.map(candyType =>
-                        <div key={`candytype--${candyType.id}`}> <h3 className="candyType">{candyType.name}</h3>
-                        {
-                            this.props.candies.filter(candy => candy.type === candyType.id)
-                            .map(candy => <div key={`candy--${candy.id}`}>{candy.name}</div>)
-                        }
+                        <div key={candyType.id} className="candyColumn">
+                            <h3 className="candyType">{candyType.name}</h3>
+                            <div className="candyHolder">
+                            {
+                                this.props.candies.filter(candy => candy.candyTypeId === candyType.id)
+                                    .map(candy =>
+                                        <div key={candy.id}>
+                                            {candy.name}
+                                            <button className="delButton" onClick={() => this.props.discontinue(candy.id)}> X </button>
+                                        </div>
+                                    )
+                                }
+                                </div>
                         </div>
                     )
                 }
+                </div>
             </section>
         )
     }
